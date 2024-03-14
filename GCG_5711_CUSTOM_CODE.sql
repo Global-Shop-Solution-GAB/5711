b04c0c8b087606939785ac35d1626b62259ffa12
Service Web <sw@gssmail.com>
2024-03-14 15:05:12 -0500

    Commit changes in C:\Websites\Git\emarlow\5711\GCG_5711_CUSTOM_CODE.sql

---
+++ GCG_5711_CUSTOM_CODE.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5711_CUSTOM_CODE
+ * ========================================== */
+CREATE TABLE "GCG_5711_CUSTOM_CODE"(
+ "CODE" CHAR(4),
+ "DESCRIPTION" CHAR(30),
+ "GSS_USER" CHAR(9),
+ "GSS_DATE" CHAR(10));
+
+
